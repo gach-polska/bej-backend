916d2a772bc06359a3ab4bc799261ba20c7ccb95
lukasz kopyszko <lukaszkopyszko98@gmail.com>
2020-09-07 17:33:53 +0200

    initial commit kopcych

---
+++ api/api.js
@@ -0,0 +1,37 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const mapRoutes = require('express-routes-mapper');
+const http = require('http');
+
+const config = require('../config/');
+const environment = process.env.NODE_ENV;
+
+const db = require('../models/index');
+
+const app = express();
+const server = http.Server(app);
+const mappedOpenRoutes = mapRoutes(config.publicRoutes, 'api/controllers/');
+
+
+// allow cross origin requests
+// configure to only allow requests from certain origins
+app.use(cors());
+
+// parsing the request bodys
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// fill routes for express application
+app.use('/', mappedOpenRoutes);
+
+server.listen(config.port, async () => {
+  console.log(`Server is listening on port ${config.port}`);
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  return db;
+});
---
+++ api/controllers/BasicController.js
@@ -0,0 +1,9 @@
+const BasicController = () => {
+    const hello = async (req, res) =>
+        res.status(200).json({hello: "Siema beju"});
+    return {
+        hello
+    }
+};
+
+module.exports = BasicController;
