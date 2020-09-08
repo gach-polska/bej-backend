261b5860470f1388ff6cabf6799ced48e6a1e6ea
lukasz kopyszko <lukaszkopyszko98@gmail.com>
2020-09-08 12:21:14 +0200

    Add friendship routes

--- config/routes/publicRoutes.js
+++ config/routes/publicRoutes.js
@@ -1,5 +1,9 @@
 const publicRoutes = {
     'GET /hello': 'BasicController.hello',
+    'GET /getUserFriendsById': 'FriendshipController.getUserFriendsById',
+    'POST /createFriendship': 'FriendshipController.createFriendship',
+    'PUT /updateFriendship': 'FriendshipController.updateFriendship',
+    'DELETE /removeFriendship': 'FriendshipController.removeFriendship',
 };
 
 module.exports = publicRoutes;
