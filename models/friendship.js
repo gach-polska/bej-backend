7106d4f019072bb61367304347002ef4adb32204
Kuba <s17838@pjwstk.edu.pl>
2020-09-08 22:20:49 +0200

    Resolve conflicts

--- models/friendship.js
+++ models/friendship.js
@@ -1,25 +1,26 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Friendship extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Friendship extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  };
-  Friendship.init({
-    userOneId: DataTypes.INTEGER,
-    userTwoId: DataTypes.INTEGER,
-    status: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Friendship',
-  });
-  return Friendship;
+    Friendship.init(
+        {
+            userOneId: DataTypes.INTEGER,
+            userTwoId: DataTypes.INTEGER,
+            status: DataTypes.TINYINT,
+        },
+        {
+            sequelize,
+            modelName: 'Friendship',
+        }
+    );
+    return Friendship;
 };
